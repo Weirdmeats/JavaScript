fa0ac89b8b116123bb3171e2925203aacbf07abe
Weirdmeats <benjaminhumberto.vergara@alumnos.ulagos.cl>
2024-05-28 10:21:59 -0400

    Updated

---
+++ sites/assets/js/js1.js
@@ -0,0 +1,41 @@
+// EJERCICIO 1: Declara una variable llamada nombre
+var nombre;
+
+//Asigna tu nombre a la variable nombre.
+nombre = "Benjamin";
+
+//Declara una variable llamada edad y asígnale tu edad.
+var edad;
+edad = 22;
+console.log(nombre + edad);
+
+
+// EJERCICIO 2:
+
+//Declara una variable de tipo número, una de tipo cadena de texto y una de tipo booleano.
+let numero = 22;
+
+//Asigna valores a cada una de estas variables.
+let Saludo = "Hola mundo mundial";
+let booleano = true;
+booleano = true;
+//Imprime el tipo de dato de cada variable usando typeof.
+console.log("Tipo de numero booleano", typeof (booleano));
+
+
+
+
+
+
+// EJERCICIO 3: Declara una constante llamada PI y asígnale el valor 3.14159.
+const pi = 3.14159;
+
+
+//Intenta cambiar el valor de PI y observa lo que sucede.
+pi = 2;
+
+//Imprime el valor de PI en la consola.
+console.log(pi);
+// Da el error "Uncaught TypeError: Assignment to constant variable. at js1.js:7:4"
+
+//EJERCICIO 4:
